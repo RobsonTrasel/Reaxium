98586b8c6aab236fb3b8c4f0f4dba61b51f68716
RobsonTrasel <robson.trasel@gmail.com>
2025-02-14 15:25:00 -0300

    feat(rust): define the main struct for Bridge

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,15 @@ struct PaceData
     interval_ms: u32,
     timer_ref: i32,
 }
+
+#[wasm_bindgen]
+pub struct SystemBridge {
+    listeners: HashMap<String, VecDeque<Function>>,
+    queues: HashMap<String, VecDeque<JsValue>>,
+    queue_limit: usize,
+    postpone_registry: HashMap<String, PostponeData>,
+    pace_registry: HashMap<String, PaceData>,
+    combined_streams: HashMap<(String, String), String>,
+}
+
+
