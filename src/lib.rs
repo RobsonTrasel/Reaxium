47dfbd830d082683f96fb43f7e062c8cf827ff16
RobsonTrasel <robson.trasel@gmail.com>
2025-02-14 15:12:19 -0300

    feat(rust): add wasm bridging for setTimeout/clearTimeout

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ use js_sys::{Function, Promise};
 use wasm_bindgen_futures::JsFuture;
 use wasm_bindgen::JsValue;
 use web_sys::window;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_name = setTimeout)]
+    fn set_timeout_closure(closure: &JsValue, ms: i32) -> i32;
+    #[wasm_bindgen(js_name = clearTimeout)]
+    fn clear_timeout(id: i32);
+}
