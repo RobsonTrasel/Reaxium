18395bff856ab5aa859c3b18b4605054b4850309
RobsonTrasel <robson.trasel@gmail.com>
2025-02-14 15:09:41 -0300

    chore: add base archive for lib :D

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use wasm_bindgen::prelude::*;
+use std::collections::{HashMap, VecDeque};
+use js_sys::{Function, Promise};
+use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen::JsValue;
+use web_sys::window;
